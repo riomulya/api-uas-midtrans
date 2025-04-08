19275f0ec7121f5b06f6510a43b974096d1b7c37
Rio Mulya Syawal <riomulya75@gmail.com>
2025-04-08 19:52:02 +0700

    ujikom

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const TransactionsRoutes = require('./routes/transactions.routes');
+const path = require('path');
 
 // const verifyToken = require('./middlewares/auth.middlewares');
 const PORT = process.env.PORT || 8080;
@@ -15,13 +16,37 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
 
+// Tambahkan middleware untuk static files
+app.use(express.static('public'));
+
 // Atau jika ingin mengurai body berdasarkan URL-encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(TransactionsRoutes);
 
+// Tambahkan route untuk download
+app.get('/download1', (req, res) => {
+  const filePath = path.join(
+    __dirname,
+    'public/assets/inventory-ujikom-pelatihan.rar'
+  );
+  res.download(filePath);
+});
+
+app.get('/download2', (req, res) => {
+  const filePath = path.join(__dirname, 'public/assets/project-ujikom.rar');
+  res.download(filePath);
+});
+
+// Update route home untuk menambahkan link download
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(`
+    <h1>Hello World</h1>
+    <a href="/download1">Full Pelatihan</a>
+    <br>
+    <br>
+    <a href="/download2">Starter Project</a>
+  `);
 });
 
 app.listen(PORT, () => {
